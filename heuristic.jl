c70944226003796e5ec46a44d52e9dbb1509446d
Louise Eymery <louise.eymery@eleves.enpc.fr>
2023-01-24 17:34:37 +0100

    shortest_path

--- heuristic.jl
+++ heuristic.jl
@@ -1,1 +1,28 @@
 # Heuristic resolution
+
+using JuMP
+using CPLEX
+
+include("instance.jl")
+
+function compute_shortest_path(instance::Instance, journey_index::Int64, taxes)
+    # Model
+    model = Model(CPLEX.Optimizer) 
+    
+    # Variables
+    @variable(model, x1[(i,j) in eachindex(instance.A1)] >= 0)
+    @variable(model, x2[(i,j) in eachindex(instance.A2)] >= 0)
+
+    # add constraints
+
+    # Objective
+    @objective(
+        model, 
+        Min, 
+        (
+            sum(x1[i, j]*(instance.A1[i, j] + get(taxes, (i, j), 0)) for (i, j) in keys(instance.A1)) 
+            + sum(x2[i, j]*instance.A2[i, j] for (i, j) in keys(instance.A2))
+        )
+    )
+
+end
