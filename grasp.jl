2356f7938c506501e637728715f85880674cd680
mathbri <mathis.brichet@gmail.com>
2023-03-07 16:54:51 +0100

    Finishing local search

--- grasp.jl
+++ grasp.jl
@@ -20,7 +20,7 @@ function generate_random_taxes(instance::Instance)
     return taxes[best_taxes, :, :], obj[best_taxes]
 end
 
-function local_search(instance::Instance, start_taxes::Matrix)
+function local_search(instance::Instance, start_taxes::Matrix, naive::Bool=false)
     taxable_arcs = collect(keys(instance.A1))
     taxes = start_taxes
     current_obj = compute_obj_value(instance, taxes, false)
@@ -28,6 +28,7 @@ function local_search(instance::Instance, start_taxes::Matrix)
     improvement = 1
     iteration = 1
     tested = 1
+    max_T_max = maximum(instance.T_max)
 
     # While we improve our objective
     while improvement > 0
@@ -36,46 +37,56 @@ function local_search(instance::Instance, start_taxes::Matrix)
         for a in shuffle(taxable_arcs)
             iteration += 1
             # For each taxable edge, a neighboor is the previous or next possible tax on this edge
-            paths_through_a = compute_paths_through_a(instance, a, taxes)
-            possible_taxes = sort!(unique(collect(values(paths_through_a))))
+            current_tax = round(taxes[a[1], a[2]])
+            possible_taxes = Float64[]
+            
+            if naive
+                # We just do +1 and -1 on the arc
+                current_tax + 1 <= max_T_max ? push!(possible_taxes, current_tax + 1) : 0
+                current_tax - 1 >= 0 ? push!(possible_taxes, current_tax - 1) : 0
+            else
+                # We compute the first inscreasing neighbor
+                paths_through_a = compute_paths_through_a(instance, a, taxes)
+                filtered_taxes = filter(item -> item > 1e-5, unique(collect(values(paths_through_a))))
+                length(filtered_taxes) > 0 ? push!(possible_taxes, current_tax + sort!(filtered_taxes)[1]) : 0
+                # We compute the first decreasing neighbor
+                if current_tax > 0
+                    taxes[a[1], a[2]] = 0
+                    paths_through_a = compute_paths_through_a(instance, a, taxes)
+                    possible_decreasing = sort!(filter(item -> item > 0, unique(collect(values(paths_through_a)))), rev=true)
+                    first_decreasing = findfirst(item -> item < current_tax, possible_decreasing)
+                    if first_decreasing !== nothing 
+                        push!(possible_taxes, possible_decreasing[first_decreasing])
+                        # println("Possible decreasing : $possible_decreasing")
+                    end
+                end
+            end
+            
+            # If no neighbor, we continue
             if length(possible_taxes) < 1
                 continue
             end
-            println("Possible taxes on $a : $(round.(Int, possible_taxes))")
-            # We get the position of our current tax in the possible taxes
-            current_tax = round(taxes[a[1], a[2]])
-            println("Current tax on $a : $current_tax")
-            current_idx = findfirst(item -> item >= current_tax, possible_taxes)
-            
-            # We construct the neighbors
-            neighbors = []
-            if current_idx === nothing 
-                # The current tax is superior to all values in possible taxes, the only neighbor is then the last value
-                push!(neighbors, length(possible_taxes))
-            else
-                if cuurent_idx == 1
-                    # The current tax is inferior to all values in possible taxes, the only neighbor is then the first value
-                    push!(neighbors, 1)
-                else
-                    # There are possible values inferior and superior to the current tax
-                    push!(neighbors, current_idx)
-                    push!(neighbors, current_idx - 1)
-                end
-            end
+            # println("Current tax on $a : $current_tax")
+            # println("Neighbor taxes on $a : $(round.(Int, possible_taxes))")
 
             # We now test both neighbors (if they exist)
-            # TODO : ADAPT FROM HERE
-            for neighbor in neighbors
-                taxes[a[1], a[2]] = possible_taxes[neighbor] - EPS
+            for neighbor in possible_taxes
+                taxes[a[1], a[2]] = neighbor- EPS
                 improvement = compute_obj_value(instance, taxes, false) - current_obj
-                println("Testing neighbor : tax $(possible_taxes[neighbor]) on arc $a with improvement $improvement")
-                if improvement <= 0
+                # println("Testing neighbor : tax $(neighbor) on arc $a with improvement $(improvement)")
+                if improvement < 0
                     # Undo changes
                     taxes[a[1], a[2]] = current_tax
                 else
+                    taxes[a[1], a[2]] = round(taxes[a[1], a[2]])
                     # Update objective
-                    current_obj += improvement
-                    println("Obj is now $current_obj")
+                    current_obj += round(improvement)
+                    if round(improvement) > 0
+                        println("Current tax on $a : $current_tax")
+                        println("Neighbor taxes on $a : $(round.(Int, possible_taxes))")
+                        println("Improving neighbor : tax $(neighbor) on arc $a with improvement $(improvement)")
+                        println("Obj is now $current_obj")
+                    end
                     # No need to check the other neighbor
                     break
                 end
@@ -101,10 +112,9 @@ end
 # Tests
 include("data/taxe_grille_7x11.txt")
 test_instance = Instance(K, n_k, n, A_1, A_2)
-# for i in 1:10
-#     random_taxes, obj = generate_random_taxes(test_instance)
-# end
-# show_positive_taxes(random_taxes)
+println(test_instance)
+random_taxes, obj = generate_random_taxes(test_instance)
+show_positive_taxes(random_taxes)
 
-n = test_instance.n
-taxes, obj = local_search(test_instance, zeros(n, n))
+taxes, obj = local_search(test_instance, deepcopy(random_taxes))
+taxes, obj = local_search(test_instance, deepcopy(random_taxes), true)
